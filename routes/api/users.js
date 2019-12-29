9dceeb8a5e21b711a480eeed4008ea577c47169c
pookiepew <pookiepew@gmail.com>
2019-12-29 20:35:36 +0000

    10. Request & Body Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const router  = express.Router()
 
-//* method    GET
+const { check, validationResult } = require('express-validator')
+
+//* method    POST
 //* route     api/users
-//* desc      Test route
+//* desc      Register new user
 //* access    Public
 //TODO        Nothing - just test
 
-router.get('/', (req, res) => res.send('Users Route'))
+router.post('/', 
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+  ], 
+  (req, res) => {
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  
+  res.send('Users Route')
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const app = express()
 
 connectDB()
 
+//* Init middleware
+
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => res.send('API Running'))
 
 
 //* Define routes
 
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/users',   require('./routes/api/users'))
+app.use('/api/auth',    require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/posts',   require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 3001
 
