f18d0893ba46d1381202d48b2d1b7596199f4a14
pookiepew <pookiepew@gmail.com>
2019-12-31 13:22:41 +0000

    22. Add & Delete Profile Education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -316,4 +316,113 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
 
 })
 
+
+
+
+
+
+
+//* method    PUT
+//* route     api/profile/education
+//* desc      Add profile education
+//* access    Private
+
+router.put('/education',
+
+  [
+    auth,
+    [
+      check('school',       'School is required')        .not().isEmpty(),
+      check('degree',       'Degree is required')        .not().isEmpty(),
+      check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+      check('from',         'From date is required')     .not().isEmpty()
+    ]
+  ],
+
+  async (req, res) => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    }
+
+    try {
+
+      const profile = await Profile.findOne({ user: req.user.id })
+
+      profile.education.unshift(newEdu)
+
+      await profile.save()
+
+      res.json(profile)
+
+    } catch (err) {
+
+      console.error(err.message)
+      res.status(500).send('Server Error')
+
+    }
+  }
+)
+
+
+
+
+
+
+
+//* method    DELETE
+//* route     api/profile/education/:edu_id
+//* desc      Delete education from profile
+//* access    Private
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+
+  try {
+
+    const foundProfile = await Profile.findOne({ user: req.user.id })
+
+    const eduIds = foundProfile.education.map(edu => edu._id.toString())
+
+    const removeIndex = eduIds.indexOf(req.params.edu_id)
+
+    if (removeIndex === -1) {
+      return res.status(500).json({ msg: "Server error" })
+    } else {
+      
+      foundProfile.education.splice( removeIndex, 1, )
+
+      await foundProfile.save()
+      return res.status(200).json(foundProfile)
+    }
+
+  } catch (error) {
+
+    console.error(error)
+    return res.status(500).json({ msg: "Server error" })
+
+  }
+})
+
 module.exports = router
