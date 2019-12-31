7407ed828079bb3db56022a80ea37b8b7ecbf655
pookiepew <pookiepew@gmail.com>
2019-12-31 12:38:25 +0000

    18. Get All Profiles & Profile By User ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,11 @@ const { check, validationResult } = require('express-validator')
 const Profile = require('../../models/Profile')
 const User    = require('../../models/User')
 
+
+
+
+
+
 //* method    GET
 //* route     api/profile/me
 //* desc      Get current users profile
@@ -31,6 +36,13 @@ router.get('/me', auth, async (req, res) => {
   }
 
 })
+
+
+
+
+
+
+
 
 //* method    POST
 //* route     api/profile
@@ -105,5 +117,98 @@ router.post('/',
 
   }
 )
+
+
+
+
+
+
+
+//* method    GET
+//* route     api/profile
+//* desc      Get all profiles
+//* access    Public
+
+router.get('/', async (req, res) => {
+
+  try {
+
+    const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+    res.json(profiles)
+
+  } catch (err) {
+
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+
+})
+
+
+
+
+
+
+
+
+//* method    GET
+//* route     api/profile/user/:user_id
+//* desc      Get profile by user id
+//* access    Public
+
+router.get('/user/:user_id', async (req, res) => {
+
+  try {
+
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar'])
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' })
+
+    res.json(profile)
+    
+  } catch (err) {
+
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' })
+    }
+    
+    res.status(500).send('Server Error')
+  }
+
+})
+
+
+
+
+
+
+
+//* method    DELETE
+//* route     api/profile/
+//* desc      Delete profile, user & posts
+//* access    Private
+
+router.delete('/', auth, async (req, res) => {
+
+  try {
+
+    // await Post.deleteMany({ user: req.user.id })
+    
+    await Profile.findOneAndRemove({ user: req.user.id })
+    
+    await User.findOneAndRemove({ _id: req.user.id })
+
+    res.json({ msg: 'User deleted' })
+
+  } catch (err) {
+
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+
+})
 
 module.exports = router
