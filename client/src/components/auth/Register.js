be7f438a22ce9b2a1138de0799dd7e78c0da5e9a
pookiepew <pookiepew@gmail.com>
2020-01-04 13:05:27 +0000

    37. Creating a Redux Store

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -14,13 +14,38 @@ const Register = () => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
 
     if ( password !== password2 ) {
       console.log('Passwords does not match');
     } else {
-      console.log(formData);
+
+      console.log('SUCCESS')
+
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password
+      // }
+
+      // try {
+        
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   };
+
+      //   const body = JSON.stringify(newUser);
+
+      //   const res = await axios.post('/api/users', body, config);
+
+      //   console.log(res.data)
+
+      // } catch (err) {
+      //   console.error(err.response.data);
+      // }
     }
   }
 
